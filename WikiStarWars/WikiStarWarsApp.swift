08c1bb8e1cddb3795e26128e137279c4c1f5941c
Albert Cuestas Casas <acuestas@amantium.net>
2022-02-04 19:17:21 +0100

    Initial Commit

---
+++ WikiStarWars/WikiStarWarsApp.swift
@@ -0,0 +1,17 @@
+//
+//  WikiStarWarsApp.swift
+//  WikiStarWars
+//
+//  Created by Albert Cuestas Casas on 4/2/22.
+//
+
+import SwiftUI
+
+@main
+struct WikiStarWarsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
